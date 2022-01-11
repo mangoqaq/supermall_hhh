dad2a6d0f8cb00c5ce8e9537c1bf877d79c442bc
mangoqaq <1399058272@qq.com>
2022-01-11 17:41:38 +0800

    home

--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -1,14 +1,45 @@
 <template>
-  <div id="">
-    <h1>首页</h1>
+  <div id="home">
+    <nav-bar class="home_nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
   </div>
 </template>
 
 <script>
-  export default {
-    name:'Home'
-  }
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childrenComps/HomeSwiper";
+import { getHomeMultidata } from "network/home";
+import HomeRecommendView from './childrenComps/HomeRecommendView'
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommendView
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      // console.log(res)
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style>
+.home_nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
